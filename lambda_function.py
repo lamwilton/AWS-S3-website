2dff1e1b51551032a8de5519068bc416c3dbe1bf
T Lam <tingfunl@usc.edu>
2020-04-01 01:27:47 +0000

    Exclude my ip

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,25 @@
 from datetime import datetime
+import boto3
+
+def writeDB(time, ip, origin):
+    db = boto3.client('dynamodb')
+    db.put_item(TableName='myWebsiteTable', Item={'time' :{'S': time}, 'ip' : {'S': ip}, 'origin' : {'S': origin}})
+
 
 def lambda_handler(event, context):
+    timeLong = datetime.now().strftime("%B %d, %Y %H:%M:%S")
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    ip = event['headers']['X-Forwarded-For']
+    origin = event['headers']['origin']
     
-    time = datetime.now().strftime("%B %d,%Y %H:%M:%S")
-
+    if ip != "128.125.146.206": # Not writing database for  myself
+        writeDB(time, ip, origin)
     resp = {
         "statusCode": 200,
         "headers": {
             "Access-Control-Allow-Origin": "*",
         },
-        "body": str(event['headers']['X-Forwarded-For']) + " Current time: " + str(time) + " GMT"
+        "body": str(ip) + " Current time: " + str(timeLong) + " GMT"
     }
-
+    
     return resp
